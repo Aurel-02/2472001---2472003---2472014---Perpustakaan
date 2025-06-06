a0eeea44b10be006646d55c851fc36884e378e20
archerriesh <2472014@maranatha.ac.id>
2025-06-06 21:58:09 +0700

    allahuakbar

--- coba.py
+++ coba.py
@@ -1,7 +1,23 @@
 import json
 
-with open('db.json', 'r', encoding='utf-8') as file:
-    database = json.load(file)
+with open('json/Anggota.json', 'r', encoding='utf-8') as file:
+    anggota = json.load(file)
+
+with open('json/buku.json', 'r', encoding='utf-8') as file:
+    buku = json.load(file)
+
+with open('json/pengunjung.json', 'r', encoding='utf-8') as file:
+    pengunjung = json.load(file)
+
+with open('json/staff.json', 'r', encoding='utf-8') as file:
+    staff = json.load(file)
+
+database = {
+    "anggota": anggota,
+    "buku": buku,
+    "pengunjung": pengunjung,
+    "staff": staff
+}
 
 def homepage():
     print ("="*50)
@@ -28,7 +44,7 @@ def homepage():
 def login_staff():
     print ("")
     print ("=== Halo staff perpustakaan Maranatha! ===")
-    inp_id = input ("Silakan masukkan ID Staff: ")
+    inp_id = str(input ("Silakan masukkan ID Staff: "))
 
     staff_bener = False
     i = 0
@@ -43,8 +59,7 @@ def login_staff():
     if staff_bener:
         staff_bener = database["staff"][i]
         print(f"Selamat datang, {staff_bener.get('nama', staff_bener.get('nama_staff', 'Staff'))}!")
-        page_staff(staff_bener.get('nama', staff_bener.get('nama_staff', 'Staff')))
-        return staff_bener
+        page_staff()
     else:
         print("ID Staff tidak ditemukan. Akses ditolak.")
         homepage()
@@ -68,88 +83,100 @@ def login_pengunjung():
         homepage()
 
 def login_mahasiswa():
-    print("=== MENU MAHASISWA ===")
-    print("1. Pinjam Buku")
-    print("2. Perpanjang Peminjaman")
-    print("3. Reservasi Buku")
-    print("4. Perpanjang keanggotaan")
-    print("0. Kembali")
+    print ("")
+    print ("=== Halo mahasiswa perpustakaan Maranatha! ===")
+    inp_id = input("Silakan masukkan ID Anggota: ")
+
+    mhs = None
+    for a in database["anggota"]:
+        if inp_id == a["id_anggota"]:
+            mhs = a
+            break
+    
+    if mhs:
+        print(f"Selamat datang, {mhs.get('nama_anggota', 'Anggota')}!")
+        page_anggota()
+    else:
+        print("ID anggota tidak ditemukan. Akses ditolak.")
+        homepage()
+
+def page_anggota ():
+    print ("")
+    print ("Silakan pilih kegiatan yang akan kamu lakukan!")
+    print ("1. Pinjam Buku")
+    print ("2. Pengembalian Buku")
+    print ("3. Perpanjang Peminjaman")
+    print ("4. Reservasi Buku")
+    print ("5. Perpanjang keanggotaan")
+    print ("0. Kembali")
 
 def menu_tamu():
     print("=== MENU TAMU ===")
     print("1. Lihat Koleksi Buku")
     print("2. Daftar Akun Pengunjung")
     print("0. Kembali")
+    homepage()
 
-def page_staff(nama):
-    while True:
-        print("")
-        print("=== Menu Staff Perpustakaan ===")
-        print(f"Selamat datang, {nama}!")
-        print("Silakan pilih kegiatan yang akan kamu lakukan:")
-        print("1. Lihat daftar buku")
-        print("2. Cek jumlah stok buku")
-        print("3. Edit kondisi buku")
-        print("4. Edit buku")
-        print("0. Keluar")
-        pilihan = int(input("Masukkan pilihan (0-4): "))
+def page_staff():
+    print("")
+    print("Silakan pilih kegiatan yang akan kamu lakukan:")
+    print("1. Lihat daftar buku")
+    print("2. Cek jumlah stok buku")
+    print("3. Edit kondisi buku")
+    print("4. Edit buku")
+    print("0. Keluar")
+    pilihan = int(input("Masukkan pilihan (0-4): "))
 
-        if (pilihan == 1):
-            lihat_daftar_buku() 
-        elif (pilihan == 2):
-            cek_jumlah_stok()  
-        elif (pilihan == 3):
-            edit_kondisi_buku() 
-        elif (pilihan == 4):
-            edit_buku(database["buku"])
-        elif (pilihan == 0):
-            print("Terima kasih, sampai jumpa kembali!")
-            homepage()
-        else:
-            print("Pilihan tidak valid. Silakan coba lagi.")
-            page_staff()
+    if (pilihan == 1):
+        lihat_daftar_buku() 
+    elif (pilihan == 2):
+        cek_jumlah_stok()  
+    elif (pilihan == 3):
+        edit_kondisi_buku() 
+    elif (pilihan == 4):
+        edit_buku(database["buku"])
+    elif (pilihan == 0):
+        print("Terima kasih, sampai jumpa kembali!")
+        homepage()
+    else:
+        print("Pilihan tidak valid. Silakan coba lagi.")
+        page_staff()
 
 def lihat_daftar_buku():
+    print("")
     print("Daftar Buku di Perpustakaan:")
     for buku in database["buku"]:
-        print(f"{buku['id_buku']} - {buku['judul_buku']} ({buku['status']})")
+        print(f"{buku['id_buku']} - {buku['judul_buku']} ({buku['stok']})({buku['status']})")
+    homepage()
 
 def cek_jumlah_stok():
+    print("")
     print("Jumlah stok buku:")
     for buku in database["buku"]:
         print(f"{buku['judul_buku']}: {buku['stok']} buah")
+    homepage()
 
 def edit_kondisi_buku():
     while True:
-        id_buku = str(input("Masukkan ID Buku yang ingin diubah kondisinya: "))
-        temu = False
+        id_buku = input("Masukkan ID Buku yang ingin diubah kondisinya: ")
+        buku_ditemukan = False
         for buku in database["buku"]:
             if (id_buku == buku["id_buku"]):
                 print(f"Kondisi saat ini: {buku['kondisi_buku']}")
                 kondisi_baru = input("Masukkan kondisi baru: ")
                 buku["kondisi_buku"] = kondisi_baru
                 print("Kondisi buku berhasil diperbarui.")
-                temu = True
-        if not temu:
+                buku_ditemukan = True
+                break
+        if not buku_ditemukan:
             print("Buku tidak ditemukan.")
 
         lanjut = input("Ingin mengubah kondisi buku lain? (y/n): ")
-        while (lanjut!="y"):
-            id_buku = str(input("Masukkan ID Buku yang ingin diubah kondisinya: "))
-            temu = False
-            for buku in database["buku"]:
-                if (id_buku == buku["id_buku"]):
-                    print(f"Kondisi saat ini: {buku['kondisi_buku']}")
-                    kondisi_baru = input("Masukkan kondisi baru: ")
-                    buku["kondisi_buku"] = kondisi_baru
-                    print("Kondisi buku berhasil diperbarui.")
-                    temu = True
-            if not temu:
-                print("Buku tidak ditemukan.")
-            lanjut = input("Ingin mengubah kondisi buku lain? (y/n): ")
-    homepage()  
-
+        if (lanjut=="n"):
+            homepage()
+    
 def edit_buku(data_buku):
+    print ("")
     print ("Apa yang ingin kamu lakukan?")
     print ("1. Tambah buku baru")
     print ("2. Hapus buku dari database")
@@ -162,7 +189,7 @@ def edit_buku(data_buku):
         for buku in data_buku:
             if (id_buku == buku["id_buku"]):
                 print("ID Buku sudah digunakan. Gunakan ID yang lain.")
-                edit_buku(database["buku"])
+                return
         
         judul = str(input("Judul Buku: "))
         tipe = str(input("Tipe buku (Buku/Buku Digital/Skripsi/Jurnal): "))
@@ -184,12 +211,12 @@ def edit_buku(data_buku):
         }
 
         data_buku.append(buku_baru)
-        with open('db.json', 'w', encoding='utf-8') as file:
-            json.dump(database, file, indent=4, ensure_ascii=False)
+        with open('json/buku.json', 'w', encoding='utf-8') as file:
+            json.dump(database["buku"], file, indent=4, ensure_ascii=False)
 
         print("Buku baru berhasil ditambahkan ke database.")
 
-    elif pilihan == "2":
+    elif (pilihan == 2):
         cari_id = input("Masukkan ID Buku yang ingin dihapus: ")
         temu = False
         for buku in data_buku:
@@ -198,8 +225,9 @@ def edit_buku(data_buku):
                 konfirmasi = str(input("Yakin ingin menghapus buku ini? (y/n): "))
                 if (konfirmasi== "y"):
                     data_buku.remove(buku)
-                    with open('db.json', 'w', encoding='utf-8') as file:
-                        json.dump(database, file, indent=4, ensure_ascii=False)
+                    with open('json/buku.json', 'w', encoding='utf-8') as file:
+                        json.dump(database["buku"], file, indent=4, ensure_ascii=False)
+
                     print("Buku berhasil dihapus dari database.")
                 else:
                     print("Penghapusan dibatalkan.")
@@ -210,6 +238,7 @@ def edit_buku(data_buku):
     else:
         print("Pilihan tidak valid.")
         edit_buku(database["buku"])
+    homepage()
 
 def main():
     homepage()
